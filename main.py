86b2ac7a376ee5dd25f3752e0d54b930e2d1bebd
bradleykabugo <138238015+bradleykabugo@users.noreply.github.com>
2023-07-28 20:21:14 +0300

    Create main.py
    first python class

---
+++ main.py
@@ -0,0 +1,34 @@
+#identation-syntax
+if 5 > 2:
+    print ("Five is greater than two")
+#variables
+userAge=20
+print (userAge)
+#defining multiple variables in one go
+userAge2, userName = 30, 'jared'
+print (userAge2)
+print (userName)
+
+x, y, z = "orange", "banana", "cherry"
+print(x)
+print(y)
+print(z)
+#assign same value to multiple variables
+a=b=c= "mango"
+print(a)
+print(b)
+print(c)
+#unpacking lists
+fruits = ["apple", "pineapple", "watermelon"]
+e, i, o = fruits
+print(e)
+print(i)
+print(o)
+
+#output multiple variables seperated by a comma
+u = "python"
+v = "is"
+w = "awersome"
+print(u, v, w)
+#use +operator to output multiple variables
+print(u + v + w)
